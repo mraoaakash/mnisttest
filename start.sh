3b68b1d28502910448c50ecb39f12975ad28506b
Aakash Rao <mrao.aakash@gmail.com>
2023-01-17 17:03:06 +0530

    Scam crao

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 #PBS -N test
 #PBS -o out.log
-#PBS -l host=compute3
-#PBS -l ngpus=1
 #PBS -e err.log
 #PBS -q gpu
 
@@ -14,4 +12,4 @@ source ~/.bashrc
 
 conda activate histoimgan
 
-python3 /home/rintu.kutum/test/mnisttest/mnist.py
+python3 /home/rintu.kutum/test/mnisttest/mnisttest.py
