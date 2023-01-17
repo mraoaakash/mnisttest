809ec62f5815d928eade46dad8c3f4f077f67155
Aakash Rao <mrao.aakash@gmail.com>
2023-01-17 15:54:32 +0530

    added main bodier code

--- mnist.py
+++ mnist.py
@@ -3,9 +3,10 @@ import torch
 
 
 # NUMBER OF GPUS   
-print(torch.cuda.device_count())
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
+if __name__ == '__main__':
+    print(torch.cuda.device_count())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
 
 
 # n_epochs = 3
