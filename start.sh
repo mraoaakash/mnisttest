7927eec7ce15faabc1b41f0dd1a1a8512bbc5ee6
Aakash Rao <mrao.aakash@gmail.com>
2023-01-17 15:52:29 +0530

    added shell

---
+++ start.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#PBS -N test
+#PBS -o out.log
+#PBS -e err.log
+#PBS -q gpu
+
+module load compiler/anaconda3
+
+conda init
+
+source ~/.bashrc
+
+conda activate histoimgan
+
+python3 /home/rintu.kutum/test/mnisttest
